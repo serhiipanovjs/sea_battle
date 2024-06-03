3578af4f44d63288b0c5b3cadb0527c23903b1ff
Serhii Panov <serhiipanovjs@gmail.com>
2024-06-03 15:26:06 +0100

    added functionality for creating fields

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,4 +15,49 @@
   const PLAYER_FIELD_ID = 'playerField';
   const COMPUTER = 'computer';
   const PLAYER = 'player';
+
+  // Function to create the game field in the DOM
+  const createField = (width, height, role, fieldId) => {
+    // Get the container element by its ID
+    const container = document.getElementById(fieldId);
+
+    // Iterate through each row
+    for (let row = 0; row < height; row++) {
+      // Create a new div element for the row
+      const fieldRow = document.createElement("div");
+      fieldRow.classList.add("fieldRow");
+
+      // Iterate through each column in the current row
+      for (let column = 0; column < width; column++) {
+
+        // Create a new div element for the cell
+        const block = document.createElement("div");
+        block.classList.add("block");
+
+        // Set a unique ID for the cell based on the role and its coordinates
+        block.id = role + String(column) + "," + String(row);
+
+        // If the role is 'COMPUTER', add a click event listener to the cell
+        if (role === COMPUTER) {
+          block.addEventListener("click", () => onComputerCellClick({x: column, y: row}));
+        }
+
+        // Append the cell to the current row
+        fieldRow.append(block);
+      }
+
+      // Append the current row to the container
+      container.append(fieldRow);
+    }
+  }
+
+// Event handler for computer's cell click
+  function onComputerCellClick(position) {
+
+  }
+
+  // Create player and computer fields
+  createField(WIDTH, HEIGHT, PLAYER, PLAYER_FIELD_ID);
+  createField(WIDTH, HEIGHT, COMPUTER, COMPUTER_FIELD_ID);
+
 })()
